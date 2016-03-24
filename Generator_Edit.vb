108344aa4204097e9adcf646bad6e1ac651d5ca7
NagaSudhir Pulla <nagasudhirpulla@gmail.com>
2016-03-24 09:37:37 +0530

    isolated  global functions into another file

--- Generator_Edit.vb
+++ Generator_Edit.vb
@@ -1,93 +1,49 @@
-''Difference between function and Sub at http://www.excel-easy.com/vba/function-sub.html
-''Scope of Variable explained at http://www.excel-easy.com/vba/examples/variable-scope.html
-Public ConsoleLineTick As Integer
-
-Public Function ClearSheet(ByVal sheetName As String)
-    Sheets(sheetName).Cells.ClearContents
-End Function
-
-Public Function ClearConsole()
-    ClearSheet "CONSOLE"
-    ConsoleLineTick = 1
-End Function
-
-Public Function WriteLineConsole(ByVal str As String)
-    Sheets("CONSOLE").Cells(ConsoleLineTick, 1).Value = str
-    ConsoleLineTick = ConsoleLineTick + 1
-End Function
-
-Public Function FieldExistsInRS( _
-   ByRef rs As ADODB.Recordset, _
-   ByVal fieldName As String)
-   Dim fld As ADODB.Field
-    
-   fieldName = UCase(fieldName)
-    
-   For Each fld In rs.Fields
-      If UCase(fld.Name) = fieldName Then
-         FieldExistsInRS = True
-         Exit Function
-      End If
-   Next
-    
-   FieldExistsInRS = False
-End Function
-Public Function Connect_To_DB(ByRef cn As ADODB.Connection)
+''TODO DECLARE GLOBAL VARIABLES LIKE TABLENAME SERVERNAME PASSWORD TABLENAME
+Sub fetch_constituent_data()
     ''https://www.reddit.com/r/excel/comments/2xpht7/vba_connection_string_to_google_cloud_sql/
     ''http://stackoverflow.com/questions/26369937/excel-vba-mysql-select-from-table-not-full-informaton
-    Dim Server_Name As String
-    Dim Database_Name As String
-    Dim User_ID As String
-    Dim Password As String
-    Dim SQLStr As String
-    Server_Name = "localhost" ' Enter your server name here
-    Database_Name = "wrldc_schedule" ' Enter your database name here
-    User_ID = "root" ' enter your user ID here
-    Password = "123" ' Enter your password here
-    cn.Open "Driver={MySQL ODBC 5.3 Unicode Driver};Server=" & Server_Name & ";Database=" & Database_Name & _
-";Uid=" & User_ID & ";Pwd=" & Password & ";"
-End Function
-
-Sub fetch_generator_data()
-    ''Connect to Schedule database
     Dim cn As ADODB.Connection
     Set cn = New ADODB.Connection
     Connect_To_DB cn
     
     ClearConsole
-    WriteLineConsole "From generator info fetch method..."
-    ' Create a recordset object.
+    WriteLineConsole "From constituent info fetch method..."
+
     Dim rsMaterialsdb As ADODB.Recordset
-    Set rsMaterialsdb = New ADODB.Recordset
-    ClearSheet "GENERATORS"
+    Dim FieldColTick As Integer
     
+    ' Create a recordset object.
+    Set rsMaterialsdb = New ADODB.Recordset
+
     With rsMaterialsdb
     ' Assign the Connection object.
     .ActiveConnection = cn
     ' Extract the required records.
-    .Open "SELECT sname, name, ramp, nunits, unitcapacity FROM generator ORDER BY generator.sname ASC"
+    .Open "SELECT sname, name FROM constituent ORDER BY constituent.sname ASC"
     
-    Dim FieldColTick As Integer
     FieldColTick = 1
     For Each fld In rsMaterialsdb.Fields
-          Sheets("GENERATORS").Cells(1, FieldColTick).Value = fld.Name
+          Sheets("CONSTITUENTS").Cells(1, FieldColTick).Value = fld.Name
           FieldColTick = FieldColTick + 1 'tick iterator
-    Next
-    Sheets("GENERATORS").Range("A2").CopyFromRecordset rsMaterialsdb
-    WriteLineConsole "Fetched the generator data"
+     Next
+    Sheets("CONSTITUENTS").Range("A2").CopyFromRecordset rsMaterialsdb
+    WriteLineConsole "Fetched the constituents data"
     ' Tidy up
     .Close
     End With
-    
-    cn.Close    'close connection to db
-    ''MARCH 21 2015 23:05 MONDAY
-    ''TODO DO THE DATA VALIDATION AT SERVER
-    ''INSERT INTO `constituent` (`idConstituent`, `name`, `sname`, `updated_at`, `connectedto`, `comments`) VALUES (NULL, 'DADRA&NAGAR HAVELI', 'DNH', CURRENT_TIMESTAMP, '', '');
-    ''SELECT * FROM `constituent` ORDER BY `constituent`.`sname` ASC
-    ''SELECT * FROM `generator` ORDER BY `generator`.`sname` ASC
+    cn.Close    'close connect to db
 End Sub
 
-Sub update_generator_data()
+Sub update_constituent_data()
+    ''https://www.reddit.com/r/excel/comments/2xpht7/vba_connection_string_to_google_cloud_sql/
+    ''http://stackoverflow.com/questions/26369937/excel-vba-mysql-select-from-table-not-full-informaton
+    ''https://dev.mysql.com/doc/connector-odbc/en/connector-odbc-examples-programming-vb-ado.html
+    ''Execute SQL Reference
+    ''https://msdn.microsoft.com/en-us/library/ms675023(VS.85).aspx
+    ''Delete all rows
+    ''sql = "TRUNCATE TABLE constituent"
+    ''Get a single field
+    ''SELECT * FROM constituent ASC LIMIT 1
     Dim colIterator As Integer
     Dim rowIterator As Integer
     Dim cellVal As String
@@ -108,7 +64,7 @@ Sub update_generator_data()
     Connect_To_DB cn
     
     ClearConsole
-    WriteLineConsole "From generator info update method..."
+    WriteLineConsole "From constituent info update method..."
     
     ' Create a recordset object.
     Set rsMaterialsdb = New ADODB.Recordset
@@ -116,16 +72,16 @@ Sub update_generator_data()
     ' Assign the Connection object.
     .ActiveConnection = cn
     ' Extract the required records.
-    .Open "SELECT * FROM generator LIMIT 1"
+    .Open "SELECT * FROM constituent LIMIT 1"
     
     canProceed = True
     FieldColTick = 1
     ''TODO find if 1st column is a unique type of column
-    Do While Trim(Sheets("GENERATORS").Cells(1, FieldColTick).Value) <> "" And canProceed = True
-          If FieldExistsInRS(rsMaterialsdb, Trim(Sheets("GENERATORS").Cells(1, FieldColTick).Value)) Then
+    Do While Trim(Sheets("CONSTITUENTS").Cells(1, FieldColTick).Value) <> "" And canProceed = True
+          If FieldExistsInRS(rsMaterialsdb, Trim(Sheets("CONSTITUENTS").Cells(1, FieldColTick).Value)) Then
           Else
             canProceed = False
-            MsgBox ("The field " & Trim(Sheets("GENERATORS").Cells(1, FieldColTick).Value) & " doesnot exist in database")
+            MsgBox ("The field " & Trim(Sheets("CONSTITUENTS").Cells(1, FieldColTick).Value) & " doesnot exist in database")
           End If
           FieldColTick = FieldColTick + 1 'tick iterator
     Loop
@@ -142,67 +98,62 @@ Sub update_generator_data()
     End If
     
     ''Construct sql string
-    ''UPDATE generator SET name = 'CGPL1', unitcapacity = '831' WHERE generator.idGenerator = 13;
     numRowsAffected = 0
     msg = "updated the rows "
     rowIterator = 2
-    Do While Trim(Sheets("GENERATORS").Cells(rowIterator, 1).Value) <> "" And rowIterator < 100
-        sql = "UPDATE generator SET "
+    Do While Trim(Sheets("CONSTITUENTS").Cells(rowIterator, 1).Value) <> "" And rowIterator < 100
+        sql = "UPDATE constituent SET "
         For colIterator = 2 To FieldColTick
-            cellVal = Trim(Sheets("GENERATORS").Cells(rowIterator, colIterator).Value)
+            cellVal = Trim(Sheets("CONSTITUENTS").Cells(rowIterator, colIterator).Value)
             If cellVal = "" Then
                 cellVal = "NULL"
             Else
                 cellVal = "'" + cellVal + "'"
             End If
-            sql = sql & Trim(Sheets("GENERATORS").Cells(1, colIterator).Value) & " = " & cellVal
+            sql = sql & Trim(Sheets("CONSTITUENTS").Cells(1, colIterator).Value) & " = " & cellVal
             If colIterator = FieldColTick Then
                 sql = sql & " "
             Else
                 sql = sql & ", "
             End If
         Next
-        sql = sql & "WHERE " & Trim(Sheets("GENERATORS").Cells(1, 1).Value) & " = '" & Trim(Sheets("GENERATORS").Cells(rowIterator, 1).Value) & "'"
+        sql = sql & "WHERE " & Trim(Sheets("CONSTITUENTS").Cells(1, 1).Value) & " = '" & Trim(Sheets("CONSTITUENTS").Cells(rowIterator, 1).Value) & "'"
         WriteLineConsole sql
         cn.Execute sql, num, adExecuteNoRecords
         If num = 1 Then
             numRowsAffected = numRowsAffected + 1
             msg = msg & CStr(rowIterator) & ", "
         ElseIf num > 1 Then
-            WriteLineConsole "Beware: not updating a unique row at row " & CStr(rowIterator) & " of GENERATORS sheet"
+            WriteLineConsole "Beware: not updating a unique row at row " & CStr(rowIterator) & " of CONSTITUENTS sheet"
         End If
         rowIterator = rowIterator + 1
     Loop
     
     If numRowsAffected > 0 Then
-        WriteLineConsole "Updated " & numRowsAffected & " rows, " & msg & "of the GENERATORS sheet"
+        WriteLineConsole "Updated " & numRowsAffected & " rows, " & msg & "of the CONSTITUENTS sheet"
     Else
         WriteLineConsole "Updated zero rows"
     End If
 
 End Sub
 
-Sub create_or_update_generator_data()
+Sub create_or_update_constituent_data()
     Dim cn As ADODB.Connection
     Set cn = New ADODB.Connection
     Connect_To_DB cn
     
     ClearConsole
-    WriteLineConsole "From generator info create or update method..."
+    WriteLineConsole "From constituent info create or update method..."
     
     Dim FieldColTick As Integer
-    
     Dim rsMaterialsdb As ADODB.Recordset
-    
     Dim canProceed As Boolean
-        
     Dim firstFieldName As String
     Dim rowIterator As Integer
     Dim colIterator As Integer
     Dim sql As String
     Dim num As Long
     Dim adExecuteNoRecords As Long
-    
     Dim cellVal As String
     Dim updateMsg As String
     Dim numRowsAffected As Integer
@@ -210,27 +161,27 @@ Sub create_or_update_generator_data()
     FieldColTick = 1
     ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
     ''Check if first column is blank and return if blank
-    If Trim(Sheets("GENERATORS").Cells(1, FieldColTick).Value) = "" Then
+    If Trim(Sheets("CONSTITUENTS").Cells(1, FieldColTick).Value) = "" Then
         MsgBox "First Column header is blank"
         Exit Sub
     End If
 
     ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-    ''fetch all the fields of the generator table and verify with the excel feild names. //TODO Check if the first column name is unique type column
+    ''fetch all the fields of the constituent table and verify with the excel feild names. //TODO Check if the first column name is unique type column
     Set rsMaterialsdb = New ADODB.Recordset
     With rsMaterialsdb
         ' Assign the Connection object.
         .ActiveConnection = cn
         ' Extract the required records.
-        .Open ("SELECT * FROM generator LIMIT 1")
+        .Open ("SELECT * FROM constituent LIMIT 1")
         canProceed = True
         
 
-        Do While Trim(Sheets("GENERATORS").Cells(1, FieldColTick).Value) <> "" And canProceed = True
-            If FieldExistsInRS(rsMaterialsdb, Trim(Sheets("GENERATORS").Cells(1, FieldColTick).Value)) Then
+        Do While Trim(Sheets("CONSTITUENTS").Cells(1, FieldColTick).Value) <> "" And canProceed = True
+            If FieldExistsInRS(rsMaterialsdb, Trim(Sheets("CONSTITUENTS").Cells(1, FieldColTick).Value)) Then
             Else
                 canProceed = False
-                WriteLineConsole "The field " & Trim(Sheets("GENERATORS").Cells(1, FieldColTick).Value) & " doesnot exist in database"
+                WriteLineConsole "The field " & Trim(Sheets("CONSTITUENTS").Cells(1, FieldColTick).Value) & " doesnot exist in database"
             End If
             FieldColTick = FieldColTick + 1 'tick iterator
         Loop
@@ -245,28 +196,28 @@ Sub create_or_update_generator_data()
         WriteLineConsole "All Fields do not exist"
         Exit Sub
     End If
-    ''fetched all the fields of the generator
+    ''fetched all the fields of the constituent
 
     ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
     ''now for each value row starting from row 2, update a row if exists or create it
-    firstFieldName = Trim(Sheets("GENERATORS").Cells(1, 1).Value)
+    firstFieldName = Trim(Sheets("CONSTITUENTS").Cells(1, 1).Value)
 
     numRowsAffected = 0
     updateMsg = "updated the rows "
     rowIterator = 2
-    Do While Trim(Sheets("GENERATORS").Cells(rowIterator, 1).Value) <> "" And rowIterator < 100
+    Do While Trim(Sheets("CONSTITUENTS").Cells(rowIterator, 1).Value) <> "" And rowIterator < 100
         ''Entered the row starting from row number 2
         ''First find if row exists using the first field
         Set rsMaterialsdb = New ADODB.Recordset
         rsMaterialsdb.ActiveConnection = cn
-        rsMaterialsdb.Open ("SELECT * FROM generator WHERE " + firstFieldName + " = '" + Trim(Sheets("GENERATORS").Cells(rowIterator, 1).Value) + "'")
+        rsMaterialsdb.Open ("SELECT * FROM constituent WHERE " + firstFieldName + " = '" + Trim(Sheets("CONSTITUENTS").Cells(rowIterator, 1).Value) + "'")
         
         If (rsMaterialsdb.BOF And rsMaterialsdb.EOF) Then
             rsMaterialsdb.Close
             ''The Row does not exist so INSERT THE ROW
-            sql = "INSERT INTO generator ("
+            sql = "INSERT INTO constituent ("
             For colIterator = 1 To FieldColTick
-                sql = sql & Trim(Sheets("GENERATORS").Cells(1, colIterator).Value)
+                sql = sql & Trim(Sheets("CONSTITUENTS").Cells(1, colIterator).Value)
                 If colIterator = FieldColTick Then
                     sql = sql & ") "
                 Else
@@ -275,7 +226,7 @@ Sub create_or_update_generator_data()
             Next
             sql = sql & "values ("
             For colIterator = 1 To FieldColTick
-                cellVal = Trim(Sheets("GENERATORS").Cells(rowIterator, colIterator).Value)
+                cellVal = Trim(Sheets("CONSTITUENTS").Cells(rowIterator, colIterator).Value)
                 If cellVal = "" Then
                     cellVal = "NULL"
                 Else
@@ -291,22 +242,22 @@ Sub create_or_update_generator_data()
         Else
             rsMaterialsdb.Close
             ''The Row exists already so UPPDATE THE ROW
-            sql = "UPDATE generator SET "
+            sql = "UPDATE constituent SET "
             For colIterator = 2 To FieldColTick
-                cellVal = Trim(Sheets("GENERATORS").Cells(rowIterator, colIterator).Value)
+                cellVal = Trim(Sheets("CONSTITUENTS").Cells(rowIterator, colIterator).Value)
                 If cellVal = "" Then
                     cellVal = "NULL"
                 Else
                     cellVal = "'" & cellVal & "'"
                 End If
-                sql = sql & Trim(Sheets("GENERATORS").Cells(1, colIterator).Value) & " = " & cellVal
+                sql = sql & Trim(Sheets("CONSTITUENTS").Cells(1, colIterator).Value) & " = " & cellVal
                 If colIterator = FieldColTick Then
                     sql = sql & " "
                 Else
                     sql = sql & ", "
                 End If
             Next
-            sql = sql & "WHERE " & firstFieldName & " = '" & Trim(Sheets("GENERATORS").Cells(rowIterator, 1).Value) & "'"
+            sql = sql & "WHERE " & firstFieldName & " = '" & Trim(Sheets("CONSTITUENTS").Cells(rowIterator, 1).Value) & "'"
         End If
         WriteLineConsole sql
         cn.Execute sql, num, adExecuteNoRecords
@@ -314,13 +265,13 @@ Sub create_or_update_generator_data()
             numRowsAffected = numRowsAffected + 1
             updateMsg = updateMsg & CStr(rowIterator) & ", "
         ElseIf num > 1 Then
-            WriteLineConsole "Beware: not updating a unique column at row " & CStr(rowIterator) & " of GENERATORS sheet"
+            WriteLineConsole "Beware: not updating a unique column at row " & CStr(rowIterator) & " of CONSTITUENTS sheet"
         End If
         rowIterator = rowIterator + 1
     Loop
 
     If numRowsAffected > 0 Then
-        WriteLineConsole "Updated " & numRowsAffected & " rows, " & updateMsg & "of the GENERATORS sheet"
+        WriteLineConsole "Updated " & numRowsAffected & " rows, " & updateMsg & "of the CONSTITUENTS sheet"
     Else
         WriteLineConsole "Updated zero rows"
     End If
